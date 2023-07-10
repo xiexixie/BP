1055a7e16ba0b7da666e662b3c48bcd051cd3544
xiexixie <xiejingnan937@qq.com>
2023-07-10 17:50:15 +0800

    done, waiting reconfiguration

--- README.md
+++ README.md
@@ -1,2 +1,71 @@
 # BP
-A simple Back propagation neural network in c++
+A simple Back propagation neural network in c++  
+## 网络结构
+本次项目的BP神经网络结构采用最简单的三层结构：  
+        
+        输入层:1 
+        隐藏层:1
+        输出层:1    
+
+## 测试实例
+`注：本项目没有模型保存功能，每次训练均为重新开始，结果可能不同`    
+
+训练数据：     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;`train_data.txt`  
+测试数据：     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;`test_data.txt`  
+输入层节点:    &nbsp;&nbsp;&nbsp;&nbsp;`IN_NODE 2`      
+隐藏层节点:    &nbsp;&nbsp;&nbsp;&nbsp;`HIDE_NODE 4`  
+输出层节点:    &nbsp;&nbsp;&nbsp;&nbsp;`OUT_NODE 1`   
+学习率:        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;`rate 0.8`  
+最大误差:      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;`theresoid 1e-4`  
+最大迭代次数:  &nbsp;`max_times 1e6`  
+<br>
+训练数据如下：  
+      
+      0 0 0
+      0 1 1
+      1 0 1
+      1 1 0
+      0.8 0.8 0
+      0.6 0.6 0
+      0.4 0.4 0
+      0.2 0.2 0
+      1.0 0.8 1
+      1.0 0.6 1
+      1.0 0.4 1
+      1.0 0.2 1
+      0.8 0.6 1
+      0.6 0.4 1
+      0.4 0.2 1
+      0.2 0 1
+      0.999 0.666 1
+      0.666 0.333 1
+      0.333 0 1
+      0.8 0.4 1
+      0.4 0 1
+      0 0.123 1
+      0.12 0.23 1
+      0.23 0.34 1
+      0.34 0.45 1
+      0.45 0.56 1
+      0.56 0.67 1
+      0.67 0.78 1
+      0.78 0.89 1
+      0.89 0.99 1
+
+<br>
+测试结果如下： 
+
+     train success with 296298 times !  
+     predict result:
+     0.111 0.112 0.0109295
+     0.001 0.999 0.999999
+     0.123 0.345 0.99999
+     0.123 0.456 0.999998
+     0.123 0.789 0.999999
+     0.234 0.567 0.999998
+     0.234 0.678 0.999998
+     0.387 0.401 0.0221967
+     0.616 0.717 0.984898
+     0.555 0.555 0.0120691
+
+
--- main.cpp
+++ main.cpp
@@ -72,7 +72,7 @@ std::vector<Sample> get_test_data(const std::string &filename)
 {
   std::vector<double> buffer = get_data(filename);
   std::vector<Sample> res;
-  for (int i = 0; i < buffer.size(); i += IN_NODE + OUT_NODE)
+  for (int i = 0; i < buffer.size(); i += IN_NODE)
   {
     Sample temp;
     for (int j = 0; j < IN_NODE; j++)
@@ -86,16 +86,19 @@ Node *input_layer[IN_NODE], *output_layer[OUT_NODE], *hidden_layer[HIDE_NODE];
 void init()
 {
   // 生成随机值
-  std::random_device seed;  // 将用于获得随机数引擎的种子
-  std::mt19937 gen(seed()); // 以 rd() 播种的标准 mersenne_twister_engine
-  std::uniform_real_distribution<> dis(-1, 1);
+  std::mt19937 rd;
+  rd.seed(std::random_device()());
+  std::uniform_real_distribution<double> dis(-1, 1);
+  // std::random_device seed;  // 将用于获得随机数引擎的种子
+  // std::mt19937 gen(seed()); // 以 rd() 播种的标准 mersenne_twister_engine
+  // std::uniform_real_distribution<> dis(-1, 1);
   // 输入层初始化
   for (int i = 0; i < IN_NODE; i++)
   {
     input_layer[i] = new Node();
     for (int j = 0; j < HIDE_NODE; j++)
     {
-      input_layer[i]->weight.push_back(dis(gen));
+      input_layer[i]->weight.push_back(dis(rd));
       input_layer[i]->weight_delta.push_back(0.f);
     }
   }
@@ -103,10 +106,10 @@ void init()
   for (int i = 0; i < HIDE_NODE; i++)
   {
     hidden_layer[i] = new Node();
-    hidden_layer[i]->bias = dis(gen);
+    hidden_layer[i]->bias = dis(rd);
     for (int j = 0; j < OUT_NODE; j++)
     {
-      hidden_layer[i]->weight.push_back(dis(gen));
+      hidden_layer[i]->weight.push_back(dis(rd));
       hidden_layer[i]->weight_delta.push_back(0.f);
     }
   }
@@ -114,7 +117,7 @@ void init()
   for (int i = 0; i < OUT_NODE; i++)
   {
     output_layer[i] = new Node();
-    output_layer[i]->bias = dis(gen);
+    output_layer[i]->bias = dis(rd);
   }
 }
 void delta_reset()
@@ -140,9 +143,12 @@ void delta_reset()
 int main()
 {
   init();
+  int count = 0;
   std::vector<Sample> train_data = get_train_data("train_data.txt");
-  for (size_t time; time < max_times; time++)
+  // train
+  for (size_t time = 0; time < max_times; time++)
   {
+    count++;
     delta_reset();
 
     double err = 0.f;
@@ -210,12 +216,101 @@ int main()
         }
         hidden_layer[i]->bias_delta += sum * hidden_layer[i]->value * (1.0 - hidden_layer[i]->value);
       }
+      // in_node weight_delta
+      for (int i = 0; i < IN_NODE; i++)
+      {
+        for (int j = 0; j < HIDE_NODE; j++)
+        {
+          double sum = 0.f;
+          for (int k = 0; k < OUT_NODE; k++)
+          {
+            sum += (idx.output[k] - output_layer[k]->value) * output_layer[k]->value * (1.0 - output_layer[k]->value) * hidden_layer[j]->weight[k];
+          }
+          input_layer[i]->weight_delta[j] += sum * hidden_layer[j]->value * (1.0 - hidden_layer[j]->value) * input_layer[i]->value;
+        }
+      }
     }
+
     if (err < thereshoid)
     {
-      std::cout << "train success with " << time + 1 << " times" << std::endl;
+      std::cout << "train success with " << time + 1 << " times !" << std::endl;
       break;
     }
+
+    // 更新weight和bias
+    auto train_data_size = double(train_data.size());
+    // 更新从input_layer到hidden_layer的weight
+    for (int i = 0; i < IN_NODE; i++)
+    {
+      for (int j = 0; j < HIDE_NODE; j++)
+      {
+        input_layer[i]->weight[j] += (rate * input_layer[i]->weight_delta[j] / train_data_size);
+      }
+    }
+    // 更新hidden_layer的bias和hidden_layer到out_layer的weight
+    for (int i = 0; i < HIDE_NODE; i++)
+    {
+      hidden_layer[i]->bias += (rate * hidden_layer[i]->bias_delta / train_data_size);
+      for (int j = 0; j < OUT_NODE; j++)
+      {
+        hidden_layer[i]->weight[j] += (rate * hidden_layer[i]->weight_delta[j] / train_data_size);
+      }
+    }
+    // 更新out_layer的bias
+    for (int i = 0; i < OUT_NODE; i++)
+    {
+      output_layer[i]->bias += (rate * output_layer[i]->bias_delta / train_data_size);
+    }
   }
+  if (count >= max_times)
+  {
+    std::cout << "train failed!";
+    return 0;
+  }
+  std::vector<Sample> test_data = get_test_data("test_data.txt");
+  std::cout << "predict result:" << std::endl;
+
+  // predict
+  for (auto &idx : test_data)
+  {
+    // 将数据输入Input_layer
+    for (int i = 0; i < IN_NODE; i++)
+    {
+      input_layer[i]->value = idx.input[i];
+    }
+    // 计算hidde_node的value
+    for (int i = 0; i < HIDE_NODE; i++)
+    {
+      double sum = 0.f;
+      for (int j = 0; j < IN_NODE; j++)
+      {
+        sum += input_layer[j]->weight[i] * input_layer[j]->value;
+      }
+      sum -= hidden_layer[i]->bias;
+      hidden_layer[i]->value = sigmoid(sum);
+    }
+    // 计算out_node的value
+    for (int i = 0; i < OUT_NODE; i++)
+    {
+      double sum = 0.f;
+      for (int j = 0; j < HIDE_NODE; j++)
+      {
+        sum += hidden_layer[j]->weight[i] * hidden_layer[j]->value;
+      }
+      sum -= output_layer[i]->bias;
+      output_layer[i]->value = sigmoid(sum);
+      idx.output.push_back(output_layer[i]->value);
+      for (auto &temp : idx.input)
+      {
+        std::cout << temp << " ";
+      }
+      for (auto &temp : idx.output)
+      {
+        std::cout << temp << " ";
+      }
+      std::cout << std::endl;
+    }
+  }
+
   return 0;
 }
